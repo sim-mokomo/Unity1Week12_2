54e4d10af85d4d1d19e7af19789e80b095a125f6
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-08 21:00:48 +0900

    【add】お金を取得した時のUI演出を実装した

---
+++ Assets/Scripts/UIAcquirePoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAcquirePoint : MonoBehaviour
+{
+    [SerializeField] private Text _point;
+
+    public void ApplyPoint(int point)
+    {
+        var prefix = point > 0 ? "+" : point < 0 ? "-" : "";
+        _point.text = $"{prefix}{point}";
+    }
+}
---
+++ Assets/Scripts/UICollentMoney.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICollentMoney : MonoBehaviour
+{
+    [SerializeField] private Text _totalMoney;
+    [SerializeField] private GameObject _acquireMoneyRoot;
+    [SerializeField] private GameObject _acquireMoneyPrefab;
+    [SerializeField] private float _acquireMoneyMoveOffset;
+    [SerializeField] private float _acquireMoneyMoveDuration;
+
+    public void ApplyTotalMoney(int totalMoney)
+    {
+        _totalMoney.text = $"{totalMoney}";
+    }
+
+    public void ApplyAcquireMoney(int acquireMoney)
+    {
+        var acquire = Instantiate(_acquireMoneyPrefab, _acquireMoneyRoot.transform.position, Quaternion.identity,
+            _acquireMoneyRoot.transform).GetComponent<UIAcquirePoint>();
+        acquire.ApplyPoint(acquireMoney);
+        acquire.transform.DOMoveY(_acquireMoneyMoveOffset, _acquireMoneyMoveDuration).onComplete =
+            () => { Destroy(acquire.gameObject); };
+    }
+}
